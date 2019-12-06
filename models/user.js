117e804ccab79c4323f8cdf6bce2c613cb3e983c
Gustavo Vasconcelos <36901821+gustavo-vasconcelos@users.noreply.github.com>
2019-12-06 10:34:28 +0000

    bcrypt js

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const saltRounds = 10;
 
 // Schema variable
