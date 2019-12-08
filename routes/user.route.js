97593222afc3f9bcbfe789cbc17fafb71cad9088
AFGRocha <antoniolaranjinha@hotmail.com>
2019-12-08 19:39:45 +0000

    user routes and controller

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,8 +3,6 @@ const router = express.Router()
 const controller = require("../Controller/user.controller")
 
 router.get("/", controller.get)
-router.post("/", controller.register)
-router.post("/login", controller.validate)
 router.put("/:id", controller.put)
 router.delete("/:id", controller.del)
 
