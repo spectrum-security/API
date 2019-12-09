cd112e4016c7c213dff9e7f9ad2d3c4ce4ac6338
Fajesse <9150221@esmad.ipp.pt>
2019-12-09 20:03:36 +0000

    added logs

---
+++ models/log.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+// Schema variable
+const Schema = mongoose.Schema;
+
+// Tags
+const logSchema = new Schema(
+  {
+    recordadedAt: {
+      type: Date,
+      required: true
+    },
+    sensorType: {
+        type:Number,
+        validate: {
+            validator: value => {
+              return [1, 2].indexOf(value) > -1; // 1:Motion sensor 2: Fingerprint sensor
+            },
+            message: "{VALUE} is not a valid type"
+          },
+          required: true
+    },
+    sensorId: {
+        type: Schema.Types.ObjectId,
+        ref: "Sensor",
+        required: true
+      },
+    userId:{
+        type:Schema.Types.ObjectId,
+        required:true
+    }
+  },
+  {
+    collection: "logs",
+    timestamps: true
+  }
+);
+
+const Log = mongoose.model("Log", logSchema);
+
+module.exports = Log;
