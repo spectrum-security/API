17fbd142ca37980683b2c3e977e1df9fb42e81b3
Diogo Miguel Melo Magalhães <iminyourpie@gmail.com>
2020-01-03 17:10:33 +0000

    Added cors, fixed minor authentication bug

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 const mongoose = require("mongoose");
 
 require("dotenv").config();
@@ -43,6 +44,7 @@ const sensorRoute = require("./routes/sensor.route");
 
 // App dependencies
 app.use(logger("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
