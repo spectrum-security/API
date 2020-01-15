9e0eb1abc9aac463b6beee7b46116d697af61046
Gustavo Vasconcelos <36901821+gustavo-vasconcelos@users.noreply.github.com>
2020-01-15 10:04:31 +0000

    bcryptjs

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,6 @@
 const UserModel = require("../models/user");
 const messages = require("../utils/jsonMessages");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
