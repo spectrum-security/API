c3e593f00f99ebea4a3fbd3dbb3cd6951b4b7b1e
Gustavo Vasconcelos <36901821+gustavo-vasconcelos@users.noreply.github.com>
2020-01-21 23:25:02 +0000

    Updated arduino controller

--- controllers/arduino.controller.js
+++ controllers/arduino.controller.js
@@ -5,46 +5,53 @@ const port = new SerialPort('COM10', {
     baudRate: 9600
 });
 
-const Log = require("../models/log")
+const Sensor = require("../models/sensor")
 
 port.pipe(parser)
 
 let logs = []
-let move = {}
 let sendData = false
 let movementStarted = false
 let movement = {}
 let limit = 10
 
-module.exports = parser.on("data", async data => {
-    let info = data.split(" ")
-    info = {
-        sensorId: info[0],
-        value: parseInt(info[1])
-    }
-    logs.push(info)
-
-
-    logs.forEach(log => {
-        if (log.value < limit) {
-            sendData = false
-            movementStarted = true
-            movement.started = new Date()
-            movement.sensorId = info.sensorId
-        } else if (log.value >= limit && movementStarted) {
-            movementStarted = false
-            movement.finished = new Date()
-            sendData = true
+listenForData = function () {
+    parser.on("data", async data => {
+        let info = data.split(" ")
+        info = {
+            sensorId: info[0],
+            value: parseInt(info[1])
         }
+        logs.push(info)
 
+
+        logs.forEach(log => {
+            if (log.value < limit) {
+                sendData = false
+                movementStarted = true
+                movement.started = new Date()
+                movement.sensorId = info.sensorId
+            } else if (log.value >= limit && movementStarted) {
+                movementStarted = false
+                movement.finished = new Date()
+                sendData = true
+            }
+
+        })
+
+        if (sendData) {
+            try {
+                const sensor = await Sensor.find({ _id: movement.sensorId })
+                sensor.logs.push(movement)
+                await sensor.save()
+                sendData = false
+                logs = []
+                console.log(sensor.logs[sensor.logs.length - 1])
+            } catch (err) {
+                console.log(err)
+            }
+        }
     })
+}
 
-    if (sendData) {
-        console.log(movement)
-        await Log.create(movement)
-        sendData = false
-        logs = []
-    }
-    console.log(info.value)
-})
-
+module.exports = { port, listenForData }
